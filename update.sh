88fcf2e36f50067bf0d17d61dc51e2345955ba8f
Colin Sullender <shiruken343@gmail.com>
2020-03-17 18:08:12 -0500

    Chain commands

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-/home/pi/berryconda3/envs/covid_choropleth/bin/python fetch.py
+/home/pi/berryconda3/envs/covid_choropleth/bin/python fetch.py && \
 git checkout gh-pages
-git add data.csv
-git commit -m "Updated `date +'%Y-%m-%d %H:%M:%S'`"
-git push
+git add data.csv &&\
+git commit -m "Updated `date +'%Y-%m-%d %H:%M:%S'`" && \
+git push &&\
 git checkout master
+
