b3ccea8df0d920df72a18a8352fa2fad91be83ee
Colin Sullender <shiruken343@gmail.com>
2020-03-17 17:47:40 -0500

    Added automatic update script

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/home/pi/berryconda3/envs/covid_choropleth/bin/python fetch.py
+git checkout gh-pages
+git add data.csv
+git commit -m "Updated `date +'%Y-%m-%d %H:%M:%S'`"
+git push
+git checkout master
