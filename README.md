5865e50804b29aaa11a617465b1434d1a1dfb8ea
Colin Sullender <shiruken343@gmail.com>
2020-03-16 23:00:24 -0500

    Added data parsing script

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # United States COVID-19 Choropleth
 
-Powered by the [2019 Novel Coronavirus COVID-19 (2019-nCoV) Data Repository by Johns Hopkins CSSE](https://github.com/CSSEGISandData/COVID-19/).
+Zoomable choropleth of confirmed COVID-19 cases in the United States. Powered by [D3.js](https://d3js.org/). Hosted on [GitHub Pages](https://pages.github.com/).
+
+https://shiruken.github.io/covid_choropleth/
+
+### Sources
+
+* Data from [USAFacts](https://usafacts.org/visualizations/coronavirus-covid-19-spread-map/)
+* FIPS state and county codes derived from [kjhealy/fips-codes](https://github.com/kjhealy/fips-codes)
---
+++ fetch.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+# Load the COVID-19 dataset from USAFacts and reformat
+df = pd.read_json("https://usafactsstatic.blob.core.windows.net/public/2020/coronavirus-timeline/allData.json")
+df.drop_duplicates(subset=['countyFIPS', 'stateFIPS'], inplace=True)
+
+# Extract most recent confirmed case counts and calculate state totals
+df['cases'] = df['confirmed'].apply(lambda x: x[-1])
+df['cases'] = df.apply(lambda x: x['cases'] if x['countyFIPS'] else df.groupby('stateAbbr')['cases'].sum()[x['stateAbbr']], axis=1)
+
+# Format FIPS codes for states
+df['id'] = df.apply(lambda x: x['countyFIPS'] if x['countyFIPS'] else x['stateFIPS']*1000, axis=1)
+
+# Load the list of state and county FIPS codes
+df_counties = pd.read_csv('fips_codes.csv')
+
+# Merge the two DataFrames together
+df = pd.merge(df_counties, df, how='left', on='id')
+df.fillna(0, inplace=True)
+df['cases'] = df['cases'].astype(int)
+
+# Copy data from Washington, DC the 'county' to District of Columbia the 'state'
+df.loc[df['id'] == 11000, 'cases'] = df[df['id'] == 11001]['cases'].values[0]
+
+# Export to file
+df.to_csv('data.csv', index=False)
