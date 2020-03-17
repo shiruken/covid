90e574f2235781b4f99ae58caef73aeda11e18dc
Colin Sullender <shiruken343@gmail.com>
2020-03-17 18:21:06 -0500

    Updated update script

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+git checkout master && \
 /home/pi/berryconda3/envs/covid_choropleth/bin/python fetch.py && \
 git checkout gh-pages && \
 git add data.csv && \
 git commit -m "Updated `date +'%Y-%m-%d %H:%M:%S'`" && \
-git push
+git push && \
 git checkout master
 
