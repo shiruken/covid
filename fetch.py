4a2e452e9bbf9771f994984d910a751c84c27d0f
Colin Sullender <shiruken343@gmail.com>
2020-06-19 18:16:47 -0400

    Calculate per capita case rate

--- fetch.py
+++ fetch.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 # Load and extract the county-level data from USAFacts
@@ -8,12 +9,15 @@ df['deaths'] = df['deaths'].apply(lambda x: x[-1])
 df.drop(df[df['county'] == 'Grand Princess Cruise Ship'].index, inplace=True)
 df.drop(df[df['county'] == 'New York City Unallocated'].index, inplace=True)
 df.drop(df[df['countyFIPS'] == 0].index, inplace=True)
-df_counties = pd.concat([df['countyFIPS'], df['cases'], df['deaths']], axis=1, keys=['FIPS', 'cases', 'deaths'])
+df_counties = pd.concat([df['countyFIPS'], df['popul'], df['cases'], df['deaths']], axis=1, keys=['FIPS', 'population', 'cases', 'deaths'])
 
 # Load and extract the state-level data from The COVID Tracking Project
 df = pd.read_json("https://covidtracking.com/api/v1/states/current.json")
 df.fillna(0, inplace=True)
 df_states = pd.concat([df['fips'], df['positive'], df['death']], axis=1, keys=['FIPS', 'cases', 'deaths'])
+
+# Calculate the state populations using the county-level data
+df_states['population'] = df_states['FIPS'].apply(lambda x: df_counties[df_counties['FIPS'].between(x*1000, (x + 1)*1000, inclusive=False)]['population'].sum())
 
 # Combine the state and county data
 df_output = df_counties.append(df_states)
@@ -26,7 +30,11 @@ df_output.loc[df_output['FIPS'] == 11001, 'deaths'] = df_output[df_output['FIPS'
 # Drop all rows where no cases or deaths were reported
 df_output = df_output[~((df_output['cases'] == 0) & (df_output['deaths'] == 0))]
 
+# Calculate the case rate per 100,000 residents
+df_output['cases_percapita'] = df_output['cases']/df_output['population']*100000
+df_output.replace(np.inf, 0, inplace=True)
+
 # Sort the rows and export to file
-df_output = df_output.astype(int)
+df_output.drop(columns='population', inplace=True)
 df_output.sort_values(by=['FIPS'], inplace=True)
-df_output.to_csv('docs/data.csv', index=False)
+df_output.to_csv('docs/data.csv', index=False, float_format='%.1f')
