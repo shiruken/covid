4561f8f93458f07ec134ab99752ce8ac7ea5337c
Colin Sullender <shiruken343@gmail.com>
2020-07-02 14:39:37 -0400

    Updated to latest USAFacts data sources

--- fetch.py
+++ fetch.py
@@ -2,14 +2,17 @@ import numpy as np
 import pandas as pd
 
 # Load and extract the county-level data from USAFacts
-df = pd.read_json("https://usafactsstatic.blob.core.windows.net/public/2020/coronavirus-timeline/allData.json")
-df.drop_duplicates(subset=['countyFIPS', 'stateFIPS'], inplace=True)
-df['cases'] = df['confirmed'].apply(lambda x: x[-1])
-df['deaths'] = df['deaths'].apply(lambda x: x[-1])
-df.drop(df[df['county'] == 'Grand Princess Cruise Ship'].index, inplace=True)
-df.drop(df[df['county'] == 'New York City Unallocated'].index, inplace=True)
+df_cases = pd.read_csv("https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv")
+df_deaths = pd.read_csv("https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_deaths_usafacts.csv")
+df_population = pd.read_csv("https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_county_population_usafacts.csv")
+df_deaths['deaths'] = df_deaths.iloc[:,-1]
+df_cases['cases'] = df_cases.iloc[:,-1]
+df = pd.merge(df_cases, df_deaths, how='left', on=['countyFIPS', 'stateFIPS', 'State'])
+df = pd.merge(df, df_population, how='left', on=['countyFIPS', 'State'])
+df.drop(df[df['County Name_x'] == 'Grand Princess Cruise Ship'].index, inplace=True)
+df.drop(df[df['County Name_x'] == 'New York City Unallocated/Probable'].index, inplace=True)
 df.drop(df[df['countyFIPS'] == 0].index, inplace=True)
-df_counties = pd.concat([df['countyFIPS'], df['popul'], df['cases'], df['deaths']], axis=1, keys=['FIPS', 'population', 'cases', 'deaths'])
+df_counties = pd.concat([df['countyFIPS'], df['population'], df['cases'], df['deaths']], axis=1, keys=['FIPS', 'population', 'cases', 'deaths'])
 
 # Load and extract the state-level data from The COVID Tracking Project
 df = pd.read_json("https://covidtracking.com/api/v1/states/current.json")
@@ -31,5 +34,6 @@ df_output.loc[df_output['FIPS'] == 11001, 'deaths'] = df_output[df_output['FIPS'
 df_output = df_output[~((df_output['cases'] == 0) & (df_output['deaths'] == 0))]
 
 # Sort the rows and export to file
+df_output = df_output.astype(int)
 df_output.sort_values(by=['FIPS'], inplace=True)
-df_output.to_csv('docs/data.csv', index=False, float_format='%.1f')
+df_output.to_csv('docs/data.csv', index=False)
