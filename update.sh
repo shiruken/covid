dd1105c03c92db901b17e043d92c50709fe12a1f
Colin Sullender <shiruken343@gmail.com>
2020-08-13 23:07:55 +0000

    Swapped to venv instead of conda

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 git checkout master && \
 git pull && \
-/home/pi/berryconda3/envs/covid_choropleth/bin/python fetch.py && \
+env/bin/python fetch.py && \
 git add docs/data.csv && \
 git commit -m "Updated `date +'%Y-%m-%d %H:%M:%S'`" && \
 git push
