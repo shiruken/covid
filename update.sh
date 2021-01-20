8732ccf00e6d4ec1b77fa21f6e1bd92e36a12e9c
Colin Sullender <shiruken343@gmail.com>
2021-01-20 16:56:19 -0500

    Delete update.sh

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git checkout master && \
-git pull && \
-env/bin/python fetch.py && \
-git add docs/data.csv && \
-git commit -m "Updated `date +'%Y-%m-%d %H:%M:%S'`" && \
-git push
-
