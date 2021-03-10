e2d6f5d75f8d4b54c0592da86543ed725ae417dd
Colin Sullender <shiruken343@gmail.com>
2021-03-09 20:57:11 -0500

    Stop using COVID Tracking Project
    COVID Tracking Project has shut down. Use USAFacts for all data now.

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ https://covid.csullender.com/
 
 ## Sources
 
-* Data from the [COVID Tracking Project](https://covidtracking.com/) and [USAFacts](https://usafacts.org/visualizations/coronavirus-covid-19-spread-map/)
+* Data from [USAFacts](https://usafacts.org/visualizations/coronavirus-covid-19-spread-map/) (Previously from [COVID Tracking Project](https://covidtracking.com/))
 * TopoJSON from [topojson/us-atlas](https://github.com/topojson/us-atlas)
 * Based on [Zoomable Choropleth by Chris Given](https://bl.ocks.org/cmgiven/d39ec773c4f063a463137748097ff52f)
 * Legend generated using [d3-legend by Susie Lu](https://d3-legend.susielu.com/)
 
 ## About the Data
 
-Data from the COVID Tracking Project are used for all state metrics. Data from USAFacts are used for all county metrics. County-level population data from USAFacts is aggregated to calculate the total state population. The "Hot Spot" metric is the average daily new cases per 100,000 people in the past week and is intended to showcase the current dynamics of COVID-19 infections (inspired by [a New York Times map](https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html)). Regions with fewer than 20 new cases over the last 7 days are not shaded.
+Data from USAFacts are used for all metrics. The "Hot Spot" metric is the average daily new cases per 100,000 people in the past week and is intended to showcase the current dynamics of COVID-19 infections (inspired by [a New York Times map](https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html)). Regions with fewer than 20 new cases over the last 7 days are not shaded.
--- fetch.py
+++ fetch.py
@@ -1,52 +1,39 @@
 import numpy as np
 import pandas as pd
 
-# Load and extract the county-level data from USAFacts
+# Load county-level data from USAFacts
 df_cases = pd.read_csv("https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv")
-df_deaths = pd.read_csv("https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_deaths_usafacts.csv")
-df_population = pd.read_csv("https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_county_population_usafacts.csv")
-
 df_cases['cases'] = df_cases.iloc[:,-1]
 df_cases['new_cases_last_week'] = df_cases.iloc[:,-1] - df_cases.iloc[:,-9]
 df_cases['new_cases_2_week_ago'] = df_cases.iloc[:,-16] - df_cases.iloc[:,-24]
+
+df_deaths = pd.read_csv("https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_deaths_usafacts.csv")
 df_deaths['deaths'] = df_deaths.iloc[:,-1]
+
+df_population = pd.read_csv("https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_county_population_usafacts.csv")
 
 df = pd.merge(df_cases, df_deaths, how='left', on=['countyFIPS', 'StateFIPS', 'State'])
 df = pd.merge(df, df_population, how='left', on=['countyFIPS', 'State'])
-df.drop(df[df['County Name_x'] == 'Grand Princess Cruise Ship'].index, inplace=True)
-df.drop(df[df['County Name_x'] == 'New York City Unallocated/Probable'].index, inplace=True)
-df.drop(df[df['countyFIPS'] == 0].index, inplace=True)
-df_counties = pd.concat([df['countyFIPS'], df['population'], df['cases'], df['new_cases_last_week'], df['new_cases_2_week_ago'], df['deaths']], 
-                        axis=1, keys=['FIPS', 'population', 'cases', 'new_cases_last_week', 'new_cases_2_week_ago', 'deaths'])
-df_counties['population'] = df_counties['population'].astype(int)
 
-# Load and extract the state-level data from The COVID Tracking Project
-df = pd.read_json("https://covidtracking.com/api/v1/states/daily.json")
-df.fillna(0, inplace=True)
-df = df.pivot(index='fips', columns='date', values=['positive', 'death']).reset_index()
-df['cases'] = df['positive'].iloc[:,-1]
-df['new_cases_last_week'] = df['positive'].iloc[:,-1] - df['positive'].iloc[:,-8]
-df['new_cases_2_week_ago'] = df['positive'].iloc[:,-15] - df['positive'].iloc[:,-22]
-df['deaths'] = df['death'].iloc[:,-1]
+# Aggregate county data to calculate state totals
+df_state = df.groupby('StateFIPS')[['cases', 'new_cases_last_week', 'new_cases_2_week_ago', 'deaths', 'population']].sum()
+df_state['countyFIPS'] = 0
+df = pd.merge(df, df_state, how='left', on=['countyFIPS', 'StateFIPS'], suffixes=[None, '_agg'])
 
-df_states = pd.concat([df['fips'], df['cases'], df['new_cases_last_week'], df['new_cases_2_week_ago'], df['deaths']], 
-                      axis=1, keys=['FIPS', 'cases', 'new_cases_last_week', 'new_cases_2_week_ago', 'deaths'])
-df_states = df_states.astype(int)
+idx_state = df['countyFIPS'] == 0
+df.loc[idx_state, 'cases'] = df['cases_agg']
+df.loc[idx_state, 'new_cases_last_week'] = df['new_cases_last_week_agg']
+df.loc[idx_state, 'new_cases_2_week_ago'] = df['new_cases_2_week_ago_agg']
+df.loc[idx_state, 'deaths'] = df['deaths_agg']
+df.loc[idx_state, 'population'] = df['population_agg']
 
-# Calculate the state populations using the county-level data
-df_states['population'] = df_states['FIPS'].apply(lambda x: df_counties[df_counties['FIPS'].between(x*1000, (x + 1)*1000, inclusive=False)]['population'].sum())
+# Format for export with unified FIPS
+df.rename(columns={'countyFIPS': 'FIPS'}, inplace=True)
+df['FIPS'] = df.apply(lambda x: x['FIPS'] if x['FIPS'] else x['StateFIPS'], axis=1)
+df = df[['FIPS', 'population', 'cases', 'new_cases_last_week', 'new_cases_2_week_ago', 'deaths']]
+df = df.astype(int)
 
-# Combine the state and county data
-df_output = df_counties.append(df_states)
+df = df[~((df['cases'] == 0) & (df['deaths'] == 0))] # Drop rows with no cases or deaths
 
-# Copy the data from Washington, DC the "state" to Washington, DC the "county"
-# Assumes that COVID Tracking Project is more up-to-date than USAFacts
-df_output.loc[df_output['FIPS'] == 11001, 'cases'] = df_output[df_output['FIPS'] == 11]['cases'].values[0]
-df_output.loc[df_output['FIPS'] == 11001, 'deaths'] = df_output[df_output['FIPS'] == 11]['deaths'].values[0]
-
-# Drop all rows where no cases or deaths were reported
-df_output = df_output[~((df_output['cases'] == 0) & (df_output['deaths'] == 0))]
-
-# Sort the rows and export to file
-df_output.sort_values(by=['FIPS'], inplace=True)
-df_output.to_csv('docs/data.csv', index=False, float_format='%d')
+df.sort_values(by=['FIPS'], inplace=True)
+df.to_csv('docs/data.csv', index=False)
